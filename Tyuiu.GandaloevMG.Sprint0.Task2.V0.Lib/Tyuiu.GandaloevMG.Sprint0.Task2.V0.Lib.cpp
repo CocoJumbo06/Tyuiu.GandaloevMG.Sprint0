a35da6b492d6bedb8b9a7a13c52ef66fd2e6e093
Tyuiu.GandaloevMG@outlook.com <mganomattermga6027@gmail.com>
2023-09-07 16:14:42 +0300

    Task2

---
+++ Tyuiu.GandaloevMG.Sprint0.Task2.V0.Lib/Tyuiu.GandaloevMG.Sprint0.Task2.V0.Lib.cpp
@@ -0,0 +1,19 @@
+// Tyuiu.GandaloevMG.Sprint0.Task2.V0.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+
+class Service :public ISprint0Task2V0
+{
+	virtual int Add(int a, int b) override
+	{
+		return a + b;
+	}
+
+
+
+
+};
